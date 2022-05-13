8544dd9521a8a14c9f0d83c804cca731b1f9b8c6
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-05-13 17:39:43 -0500

    Current weather function append works

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,4 @@
 var APIKey = "0544d7752b7cf21b57b19a2cc047173f"
-var city;
-var state;
-var country;
 
 var citySearchEl = $("#city-search");
 var popularCityEl = $("#popular-city")
@@ -13,6 +10,7 @@ function handleSearchData(event) {
     var currentInput = btnClicked.parent().children()[0].value;
     
     console.log(currentInput);
+    getCurrentConditions();
 }
 
 function handlePopularCityClick(event) {
@@ -21,7 +19,41 @@ function handlePopularCityClick(event) {
     var btnClicked = event.target.getAttribute('data-language');
 
     console.log(btnClicked);
+    getCurrentConditions();
 }
+
+
+function getCurrentConditions(event) {
+    var city = $('#search-city').val();
+    
+    var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&appid=' + APIKey;
+    fetch(requestURL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then (function(data) {
+            console.log(data);
+
+            var iconCode = data.weather[0].icon;
+            var iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
+
+            var currentCity = $(`
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h2 class="subtitle" id="cityWeatherToday">
+                            ${data.name} <img src='${iconURL}' alt='${data.weather[0].description}'>
+                         </h2>
+                        <p>Temperature: ${data.main.temp} Degrees F</p>
+                        <p>Humidity: ${data.main.humidity}%</p>
+                        <p>Wind Speed: ${data.wind.speed} MPH</p>
+                    </div>
+                </div>
+            `);
+
+             $("#currentCity").append(currentCity);
+        })
+}
+
 
 //Event listener for city search input
 citySearchEl.on('click', '.btn', handleSearchData)
@@ -29,12 +61,13 @@ citySearchEl.on('click', '.btn', handleSearchData)
 //Event listener for popular city buttons
 popularCityEl.on('click', '.btn', handlePopularCityClick)
 
+//ESSENTIAL TO DOS
+//TODO: Create local storage data for recent searches
+//TODO: Create append function for recent searches to the sidebar
+//TODO: Successfully pull API data from Open Weather Map for current day
+//TODO: Create loop for 5 day forecast, rendering to the cards
+//Readme updated
 
-
-
-// var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-// fetch(queryURL);
-
-
-
+//STYLE BONUS
+//Improve UI with more custom look/feel
+//Figure out FontAwesome
